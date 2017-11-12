c82e4312f6d8b31bbe2bae4a996793fdcc5296b3
Chris R <git@cdr.fastmail.fm>
2017-11-12 14:10:27 +0000

    version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='faktory',
-      version='0.1',
+      version='0.2',
       description='Python worker for the Faktory project',
       classifiers=[
           'Development Status :: 3 - Alpha',
