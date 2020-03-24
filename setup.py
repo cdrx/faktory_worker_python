d08664942d23d18137c581070b80bd1f6a714739
Alex Cano <alexander.cano@seriously.com>
2020-03-24 16:25:06 -0700

    create dev and testing requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
+import sys
 from setuptools import setup
 
-setup(name='faktory',
-      version='0.4.0',
-      description='Python worker for the Faktory project',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 3',
-          'Topic :: System :: Distributed Computing'
-      ],
-      keywords='faktory worker',
-      url='http://github.com/cdrx/faktory_python_worker',
-      author='Chris R',
-      license='BSD',
-      packages=['faktory'],
-      zip_safe=False)
+dev_requires = open("dev-requirements.txt").read().strip().split("\n")
+test_requires = open("test-requirements.txt").read().strip().split("\n")
+
+extras = {"dev": dev_requires + test_requires, "test": test_requires}
+
+if sys.version_info < (3, 6):
+    extras["dev"].remove("black")
+
+extras["all_extras"] = sum(extras.values(), [])
+
+setup(
+    name="faktory",
+    version="0.4.0",
+    description="Python worker for the Faktory project",
+    extras_require=extras,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Distributed Computing",
+    ],
+    keywords="faktory worker",
+    url="http://github.com/cdrx/faktory_python_worker",
+    author="Chris R",
+    license="BSD",
+    packages=["faktory"],
+    zip_safe=False,
+)
